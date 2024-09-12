c02c45a5d5bac2b395ab5b1b27d9559948cc60ad
michael-3mad87 <michael.3mad87@gmail.com>
2024-09-12 21:42:15 +0300

    create MVVM folder & AppTheme

---
+++ lib/shared/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xff121312);
+  static const Color grey = Color(0xff282A28);
+  static const Color bottomNavColor = Color(0xff1A1A1A);
+  static const Color white = Color(0xffffffff);
+  static const Color gold = Color(0xffFFBB3B);
+  static const Color searchColor = Color(0xff514F4F);
+  static  ThemeData darkTheme = ThemeData();
+  
+}
